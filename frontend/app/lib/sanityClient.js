70f479e3a8e0c3b6ee6da418e6571fa52d5ad71a
Joseph Smith <josephsmi2013@gmail.com>
2023-09-25 13:50:07 -0600

    hard coded the projID

--- frontend/app/lib/sanityClient.js
+++ frontend/app/lib/sanityClient.js
@@ -1,7 +1,8 @@
 import { createClient } from 'next-sanity';
 
 const sanity = createClient({
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	//projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	projectId: 'ri01u2se',
 	dataset: 'production',
 	apiVersion: '2021-03-25',
 	useCdn: true,
